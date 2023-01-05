205ba50880fc0a71ebfd2783b97d91bb8556bb69
Alexander Parco <alexparco16@gmail.com>
2023-01-05 14:38:49 -0500

    change host  connection to mysql

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,9 @@ import { Connection, createConnection } from 'mysql2'
 
 export function connect(): Connection {
     const connection = createConnection({
-        host: 'localhost',
+        host:   '127.0.0.1',
         user: 'root',
+        password: 'password',
         database: 'tasks_db',
     });
     return connection;

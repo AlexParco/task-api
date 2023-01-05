e539a5897e9afa39e3d51805598103650a34960e
Alexander Parco <alexparco16@gmail.com>
2023-01-05 15:30:01 -0500

    add delete, put and save

--- src/service/task.service.ts
+++ src/service/task.service.ts
@@ -1,14 +1,15 @@
+import { Connection, OkPacket, QueryError } from 'mysql2'
+import { connect } from '../db'
 import Task from '../models/task'
-import {Connection, QueryError} from 'mysql2'
 
 export default class TaskService{
-    private conn: Connection
+    conn: Connection
 
-    constructor(conn: Connection) {
-        this.conn = conn
+    constructor() {
+        this.conn = connect()
     }
 
-    public findAllTasks(): Promise<Task[]> {
+    findAll(): Promise<Task[]> {
         return new Promise((resolve, reject) => {
             this.conn.query<Task[]>(
                 "SELECT * FROM tasks",
@@ -20,16 +21,60 @@ export default class TaskService{
         })
     }
 
-    public findTaskById(id: string): Promise<Task> {
+    public findById(id: number): Promise<Task> {
         return new Promise((resolve, reject) => {
-        this.conn.query<Task[]>(
-            "SELECT * FROM tasks WHERE id = ?",
-            [id],
-            (err: QueryError | null, res: Task[]) => {
-                err ? reject(err)
-                : resolve(res?.[0])
+            this.conn.query<Task[]>(
+                "SELECT * FROM tasks WHERE id=?",
+                [id],
+                (err: QueryError | null, res: Task[]) => {
+                    err ? reject(err)
+                    : resolve(res?.[0])
             })
         })
     }
 
+    public save(task: Task): Promise<Task> {
+        task.expired_at = new Date(task.expired_at)
+        return new Promise((resolve, reject) => {
+            this.conn.query<OkPacket>(
+                "INSERT INTO tasks (name, description, expired_at) VALUES (?, ?, ? )",
+                [task.name, task.description, task.expired_at],
+                (err: QueryError | null, res: OkPacket) => {
+                    err ? reject(err)
+                    : this.findById(res.insertId)
+                        .then(res => resolve(res))
+                        .catch(err => reject(err))
+            })
+        })
+    }
+
+    public async update(task: Task, id: number): Promise<Task> {
+        const taskLoad = await this.findById(id)
+        task.created_at = taskLoad.created_at
+
+        return new Promise((resolve, reject) => {
+            this.conn.query<OkPacket>(
+                "UPDATE tasks SET name=?, description=?, created_at=?, expired_at=? WHERE id=?",
+                [task.name, task.description, task.created_at, task.expired_at, id],
+                (err: QueryError | null , res: OkPacket) => {
+                    err ? reject(err)
+                    : this.findById(task.id)
+                    .then(res => resolve(res))
+                    .catch(err => reject(err))
+                }
+            )
+        })
+    }
+
+    public delete(id: number): Promise<number> {
+        return new Promise((resolve, reject) => {
+            this.conn.query<OkPacket>(
+                "DELETE FROM tasks WHERE id = ?",
+                [id],
+                (err: QueryError | null, res: OkPacket) => {
+                    err ? reject(err)
+                    : resolve(res.affectedRows)
+            })
+        })
+    }
 }
